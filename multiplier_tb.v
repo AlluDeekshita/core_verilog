3e250802b25e3778969d54750396121612d3d1d8
AlluDeekshita <142439451+AlluDeekshita@users.noreply.github.com>
2024-08-13 15:15:18 +0530

    Update multiplier_tb.v

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,35 +1,32 @@
-`include "fmultiplier.v"
-module fmultiplier_tb;
-
-reg [31:0] a, b;
-wire [31:0] z;
-reg clk, rst;
-
-fmultiplier multiplier(clk , rst, a, b, z);
-
-initial begin
-	$dumpfile("wave.vcd");
-	$dumpvars(0, fmultiplier_tb);
+`timescale 1ns/1ps
+ module 32_bit_multiplier_tb
+  reg [31:0] A;
+  reg [31:0] B;
+  wire [31:0] C;
+  32_bit_multiplier m1(.A(A),.B(B),.(C));
+  initial
+  begin 
+  A[0] = 32'b0_01111111_10000000000000000000000;  // 1.5
+  B[0] = 32'b1_10000000_10000000000000000000000;  // -3.0
+  
+ #100
+  
+  A[1] = 32'b1_10000000_01100000000000000000000;  //-2.75
+  B[1] = 32'b0_10000000_00000000000000000000000;  // 2.0
+  
+// Input elements of array A and B.
+  
 end
 
-initial begin
-	clk <= 0;
-	rst <= 1;
-	repeat(17000)
-		#5 clk <= ~clk;
+initial
+begin
+
+#600
+  $display(" Result : %b",result);
+  //Displaying the final output of PE.
+#600
+
+$finish;
 end
-
-initial #13 rst <= 0;
-
-initial begin
-	#3
-	repeat(500) begin
-		#80
-		a = $random;
-		b = $random;
-	end
-	#80 $finish;
-end
-
-
 endmodule
+
