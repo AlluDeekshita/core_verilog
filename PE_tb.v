dc5fa8195a2bc14a81ec8282932f0753b28493be
AlluDeekshita <142439451+AlluDeekshita@users.noreply.github.com>
2024-11-13 07:18:18 +0530

    Update and rename multiplier_tb.v to PE_tb.v

--- PE_tb.v
+++ PE_tb.v
@@ -1,15 +1,23 @@
-`timescale 1ns/1ps
- module 32_bit_multiplier_tb
-  reg [31:0] A;
-  reg [31:0] B;
-  wire [31:0] C;
-  32_bit_multiplier m1(.A(A),.B(B),.(C));
-  initial
-  begin 
+`timescale 1ns / 1ps
+`define N 2
+//This is the size of array we are passing through test bench. Be careful to change the same 
+
+module TB;
+  reg [31:0] A [0:`N-1]; //Input array of N elements A.
+  reg [31:0] B [0:`N-1]; //Input array of N elements B.
+  wire [31:0] result; //Output of PE.
+  
+  PE F_Add (A,B,result); //DUT Instantiation.
+
+initial  
+begin
+  
+  //Stimulus generation
+
   A[0] = 32'b0_01111111_10000000000000000000000;  // 1.5
   B[0] = 32'b1_10000000_10000000000000000000000;  // -3.0
   
- #100
+  #100
   
   A[1] = 32'b1_10000000_01100000000000000000000;  //-2.75
   B[1] = 32'b0_10000000_00000000000000000000000;  // 2.0
@@ -29,4 +37,3 @@ begin
 $finish;
 end
 endmodule
-
